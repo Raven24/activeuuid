a91fb4b124cf9a95c2398d47554f148617b962e9
Florian Staudacher <florian_staudacher@yahoo.de>
2011-11-02 21:29:46 +0100

    added initial support for SQLite databases

--- lib/activeuuid.rb
+++ lib/activeuuid.rb
@@ -1,6 +1,7 @@
 require "activeuuid/version"
 
 module ActiveUUID
+  require 'uuidtools'
   require 'activeuuid/railtie' if defined?(Rails)
   require 'activeuuid/uuid'
 end
--- lib/activeuuid/railtie.rb
+++ lib/activeuuid/railtie.rb
@@ -10,8 +10,9 @@ module ActiveUUID
         class TableDefinition
           def uuid (*args)
             options = args.extract_options!
+            options[:limit] = 16
             column_names = args
-            column_names.each { |name| column(name, 'binary(16)', options) }
+            column_names.each { |name| column(name, :binary, options) }
           end                                                                     
         end
       end
